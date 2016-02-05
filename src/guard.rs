22f68a917d7210d026ddff37a76991720deb6c6f
HaronK <okreptul@yahoo.com>
2016-02-05 17:43:08 +0100

    Initial Guard wrapper

---
+++ src/guard.rs
@@ -0,0 +1,28 @@
+﻿
+use std::sync::Mutex;
+
+pub struct Guard<T: Drop> {
+    obj: T,
+    mutex: Mutex,
+}
+
+impl<T> Guard<T> {
+    #[inline]
+    pub fn new(obj: T) -> Guard<T> {
+        let res = Guard {
+            obj: obj,
+            mutex: Mutex::new(obj),
+        }
+        res.mutex.lock().unwrap();
+    }
+}
+
+impl<T> Drop for Guard<T> {
+    fn drop(&mut self) {
+        while let Some(mut head_) = self.head.take() {
+            self.head = head_.next.take();
+        }
+        self.length = 0;
+        self.tail = Rawlink::none();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod writer_list;
 pub mod reader_list;
 
 pub mod pipe;
+pub mod guard;
 
 #[test]
 fn it_works() {}
